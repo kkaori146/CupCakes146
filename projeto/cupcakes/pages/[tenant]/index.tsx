5f5316780810dc8b5fd1379014feaa7f62337c1f
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2024-09-07 18:57:27 -0300

    Tela de Cardápio

--- projeto/cupcakes/pages/[tenant]/index.tsx
+++ projeto/cupcakes/pages/[tenant]/index.tsx
@@ -1,9 +1,32 @@
+import { SearchInput } from '../../components/SearchInput';
 import styles from '../../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Seja bem-vindo(a)!!</h1>
+    <div className= {styles.container}>
+      
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem-Vindo(a)👋</div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje?</div>
+          </div>
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButton}>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerBottom}>
+          <SearchInput
+          
+          />
+
+        </div>
+
+      </header>
     </div>
   );
 }

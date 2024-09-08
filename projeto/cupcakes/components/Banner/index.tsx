6662a73db3be0c397aef86ca9ed4fdaf1cceff62
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2024-09-08 11:37:26 -0300

    Banner

--- projeto/cupcakes/components/Banner/index.tsx
+++ projeto/cupcakes/components/Banner/index.tsx
@@ -1,20 +1,25 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
+
 import styles from './styles.module.css';
 
 export const Banner = () => {
     return (
         <div className={styles.container}>
             <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+                slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay:2000,
+                    disableOnInteraction:false
+                }}
+                modules={[Autoplay]}
+                className={styles.swiper}
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    ...
+                    <SwiperSlide className={styles.slide}><img src="/tmp/banner1.png" alt=''/></SwiperSlide>
+                    <SwiperSlide className={styles.slide}><img src="/tmp/banner2.png" alt=''/></SwiperSlide>
             </Swiper>
         </div>
     );

da4f955808268022a3a26986e274f0a709f695f3
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2024-09-09 08:06:42 -0300

    Criação do Context para os Tenants

--- projeto/cupcakes/pages/_app.tsx
+++ projeto/cupcakes/pages/_app.tsx
@@ -1,6 +1,12 @@
+import { AppContextProvider } from "@/contexts/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+function MyApp ({Component, pageProps}: AppProps) {
+  return (
+    <AppContextProvider>
+      <Component {...pageProps} />
+    </AppContextProvider>
+  );
+} 
+export default MyApp;

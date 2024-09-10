7478ef7a3a3deca1bf06b98a1bd1c53e308ad69f
Katia Kaori Kaminishikawahara <kkaori146@gmail.com>
2024-09-10 18:25:58 -0300

    Tela de cadastros route

--- projeto/cupcakes/pages/[tenant]/login.tsx
+++ projeto/cupcakes/pages/[tenant]/login.tsx
@@ -8,6 +8,8 @@ import Head from 'next/head';
 import { Header } from '../../components/Header';
 import { InputField } from '../../components/InputField';
 import { Button } from '../../components/Button';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Login = (data: Props) => {
   const { tenant, setTenant } = useAppContext();
@@ -16,12 +18,14 @@ const Login = (data: Props) => {
     setTenant(data.tenant);
   }, []);
 
+  const router = useRouter();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const handleSubmit = () => {
   }
   const handleSignUp = () => {
+    router.push(`/${data.tenant.slug}/signup`);
     
   }
 
@@ -38,7 +42,14 @@ const Login = (data: Props) => {
 
           <div className={styles.header}>{data.tenant.name}</div>
 
-          <div className={styles.subtitle}>Use suas credenciais para realizar o login</div>
+          <div
+            className={styles.subtitle}
+            style={{borderBottomColor: data.tenant.name}}
+          >Use suas credenciais para realizar o login</div>
+
+          <div className={styles.line}></div>
+
+
 
           <div className={styles.formArea}>
             
@@ -69,9 +80,13 @@ const Login = (data: Props) => {
               />
             </div>
           </div>
-          <div className={styles.forgetArea}>
-            Esqueceu sua senha? Clique aqui
+          <div
+            className={styles.forgetArea}
+            style={{borderBottomColor: data.tenant.mainColor}}
+          >
+            Esqueceu sua senha? <Link legacyBehavior href={`/${data.tenant.slug}/forget`}><a style={{color: data.tenant.mainColor}}>Clique aqui</a></Link>
           </div>
+          <div className={styles.line}></div>
           <div className={styles.signupArea}>
             <Button
               color={data.tenant.mainColor}
